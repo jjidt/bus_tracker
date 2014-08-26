98051d0591c9158efe3f98711d317ad5f2bfe6fb
Justin Speers & J.J. Idt <student@epicodus.com>
2014-08-26 11:49:14 -0700

    validate presence of name for Line

---
+++ app/controllers/lines_controller.rb
@@ -0,0 +1,22 @@
+class LinesController < ApplicationController
+
+  def index
+    @lines = Line.all
+  end
+
+  def new
+    @line = Line.new
+  end
+
+  def create
+    @line = Line.new(params[:line])
+    if @line.save
+      flash[:notice] = 'successfully saved'
+      redirect_to lines_path(@line)
+    else
+      flash[:alert] = 'please fix the following errors'
+      render 'new'
+    end
+  end
+
+end
--- app/models/line.rb
+++ app/models/line.rb
@@ -1,3 +1,3 @@
 class Line < ActiveRecord::Base
-
+  validates_presence_of :name
 end
