be4624aba95665d764c3959cc20921db509f3530
Justin Speers & J.J. Idt <student@epicodus.com>
2014-08-26 16:05:21 -0700

    create MVC for stations

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,4 +4,52 @@ class StationsController < ApplicationController
     @stations = Station.all
   end
 
+  def new
+    @station = Station.new
+  end
+
+  def create
+    @station = Station.new(line_params)
+
+    if @station.save
+      flash[:notice] = "Created New Station"
+      redirect_to station_path(@station)
+    else
+      flash[:alert] = "Error creating new station"
+      render('new')
+    end
+  end
+
+  def show
+    @station = Station.find(params[:id])
+  end
+
+  def edit
+    @station = Station.find(params[:id])
+  end
+
+  def update
+    @station = Station.find(params[:id])
+
+    if @station.update(line_params)
+      flash[:notice] = "Sucessfully updated station"
+      redirect_to station_path(@station)
+    else
+      flash[:alert] = "Error updating station"
+      render('edit')
+    end
+
+  end
+
+  def destroy
+    @station = Station.find(params[:id])
+    @station.destroy
+    redirect_to root_path
+  end
+
+private
+  def line_params
+    params.require(:station).permit(:name)
+  end
+
 end
