0c6a9ed3f9cc7cda2335768b112cae6c9f3fc2e6
Justin Speers & J.J. Idt <student@epicodus.com>
2014-08-26 15:32:04 -0700

    nested relations

--- app/models/line.rb
+++ app/models/line.rb
@@ -2,4 +2,5 @@ class Line < ActiveRecord::Base
   has_many :stops
   has_many :stations, through: :stops
   validates_presence_of :name
+
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -2,4 +2,6 @@ class Station < ActiveRecord::Base
   has_many :stops
   has_many :lines, through: :stops
   validates_presence_of :name
+
+
 end
