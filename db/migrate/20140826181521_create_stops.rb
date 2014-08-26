f19956c834666b29f0b01e1b63583df727bd6bc7
Justin Speers & J.J. Idt <student@epicodus.com>
2014-08-26 11:16:35 -0700

    create stops join table for lines/stations

---
+++ db/migrate/20140826181521_create_stops.rb
@@ -0,0 +1,9 @@
+class CreateStops < ActiveRecord::Migration
+  def change
+    create_table :stops do |t|
+      t.integer :station_id
+      t.integer :line_id
+
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826181204) do
+ActiveRecord::Schema.define(version: 20140826181521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20140826181204) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "stops", force: true do |t|
+    t.integer "station_id"
+    t.integer "line_id"
   end
 
 end
